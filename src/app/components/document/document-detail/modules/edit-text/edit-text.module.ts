def7bccd9f3e317a00135d9b3a6b52c3807c929d
codeshanks <103949989+codeshanks@users.noreply.github.com>
2022-05-13 14:40:36 -0500

    may 12

--- src/app/components/document/document-detail/modules/edit-text/edit-text.module.ts
+++ src/app/components/document/document-detail/modules/edit-text/edit-text.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditTextComponent } from './edit-text.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [EditTextComponent],
   exports: [EditTextComponent]
